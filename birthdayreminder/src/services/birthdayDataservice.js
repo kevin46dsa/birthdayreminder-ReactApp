3bc115cd6e166b85823ee1311e18b47fe23275dd
Kevin dsa <kevin0108dsa@gmail.com>
2022-08-24 13:30:01 -0400

    Create birthdayDataservice.js

---
+++ birthdayreminder/src/services/birthdayDataservice.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+import config from '../Config/envVariables';
+const API_URL = `${config.Server_url}/users`;
+//const Public_URL = `${config.Server_url}/public`;
+
+const getBirthday = () => {
+	return axios
+		.get(API_URL + '/checkbirthday', { headers: authHeader() })
+		.then((response) => {
+			return response.data.data[0];
+		});
+};
+
+const BirthdayService = {
+	getBirthday,
+};
+export default BirthdayService;
